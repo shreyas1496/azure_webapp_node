f0b3eea96b620e40e4dc891a894a738fbbbce510
Shreyas Patankar <ssp1996@gmail.com>
2020-07-18 18:34:42 +0530

    Fixed

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
-const express = require('express');
+const express = require("express");
+const websocket = require("websocket");
+const http = require("http");
 const app = express();
 
-const path = require('path')
+const path = require("path");
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 //body parser middleware
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.get('/',(req,res)=>{
-    res.render('home.ejs')
-})
+app.get("/home", (req, res) => {
+  res.render("home.ejs");
+});
 
-app.post('/',(req,res)=>{
-    console.log(req.body.name,req.body.stuff);
+app.post("/home", (req, res) => {
+  console.log(req.body.name, req.body.stuff);
 
-    res.render('welcome.ejs',{
-        name: req.body.name,
-        stuff:req.body.age
-    })
-})
+  res.render("welcome.ejs", {
+    name: req.body.name,
+    stuff: req.body.age,
+  });
+});
 
-app.listen(process.env.PORT||5000,console.log('5000'))
+const server = http.createServer();
+
+server.listen(9898);
+
+const wsServer = new websocket.server({
+  httpServer: server,
+});
+
+wsServer.on("request", (request) => {
+  const connection = request.accept(undefined, request.origin);
+  console.log("joined");
+
+  connection.on("message", async (post) => {
+    const { from, message } = JSON.parse(post.utf8Data || "");
+    wsServer.broadcastUTF(post.utf8Data);
+  });
+  connection.on("close", () => {
+    console.log("Client has disconnected.");
+  });
+});
+
+app.listen(process.env.PORT || 5000, console.log("5000"));

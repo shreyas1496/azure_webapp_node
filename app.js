4e51a0faa738c713c1f4c2cc0cf77d4bdd7508cd
Shreyas Patankar <ssp1996@gmail.com>
2020-07-18 18:50:22 +0530

    Fixed port

--- app.js
+++ app.js
@@ -1,36 +1,9 @@
-const express = require("express");
 const websocket = require("websocket");
 const http = require("http");
-const app = express();
-
-const path = require("path");
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-//body parser middleware
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
-app.get("/home", (req, res) => {
-  res.render("home.ejs");
-});
-
-app.post("/home", (req, res) => {
-  console.log(req.body.name, req.body.stuff);
-
-  res.render("welcome.ejs", {
-    name: req.body.name,
-    stuff: req.body.age,
-  });
-});
 
 const server = http.createServer();
 
-server.listen(9898);
+server.listen(process.env.PORT || 443, console.log("5000"));
 
 const wsServer = new websocket.server({
   httpServer: server,
@@ -49,4 +22,4 @@ wsServer.on("request", (request) => {
   });
 });
 
-app.listen(process.env.PORT || 5000, console.log("5000"));
+wsServer.on("error", console.log);
